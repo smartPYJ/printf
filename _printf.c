25c483c963a0926b937000d62cf5edcff16538d5
meetkome <kvictory1010@gmail.com>
2022-07-11 16:56:14 -0700

    new commit

--- _printf.c
+++ _printf.c
@@ -12,17 +12,7 @@ int _printf(const char *format, ...)
 	conver_t f_list[] = {
 		{"c", print_char},
 		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
+		{"%", print_percent}
 	};
 	va_list list_arg;
 
