bc4a7bc33501870ee5aed1ec736f06d0a7f8b1dd
meetkome <kvictory1010@gmail.com>
2022-07-11 16:02:10 -0700

    main h

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - a function That produces output according to format.
+ * @format: a character string
+ * Returns: The number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+
+/*Main functions*/
+int _printf(const char *format, ...);
+int _putchar(char c)
+
+#endif
