3b87b9c904635952f896da28d03a4652309dc714
meetkome <kvictory1010@gmail.com>
2022-07-12 08:39:01 -0700

    parser function

--- _printf.c
+++ _printf.c
@@ -14,15 +14,6 @@ int _printf(const char *format, ...)
 		{"s", print_string},
 		{"%", print_percent},
 		{"d", print_integer},
-		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{NULL, NULL}
 	};
 	va_list arg_list;
 
