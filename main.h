5cf30cf28a4ccb3e0ab7798cbcec588e1936fa7f
meetkome <kvictory1010@gmail.com>
2022-07-12 07:26:39 -0700

    New commit

--- main.h
+++ main.h
@@ -21,7 +21,9 @@ typedef struct convert conver_t;
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
-
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
 
 
 /*Supporting functions*/
