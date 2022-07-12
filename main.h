6e21afb8e37d41d18aadee913a09e768fe3d0729
meetkome <kvictory1010@gmail.com>
2022-07-12 10:01:57 -0700

    new commit

--- main.h
+++ main.h
@@ -5,23 +5,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-/**
-* struct convert - A structure for functions and symbols
-* @sym: The operator
-* @f: pointer to a printer functions
-*
-* Description: struct that stores pointers to a
-* printer functions.
-*/
-
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
-
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 
